a619492501ef486841b42162345e227d9251e377
ochiaihitomi <sky.force.1013@gmail.com>
2021-02-04 14:19:10 +0000

    [Update] 検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'homes#top'
   get '/home/about' => 'homes#about'
@@ -9,15 +10,13 @@ Rails.application.routes.draw do
       get 'follows'
       get 'followers'
     end
-    
   end
   
-
-
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 
+  get '/search' => 'search#search'
 
 end
