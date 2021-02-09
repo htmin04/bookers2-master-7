f4972a57cb3e83f69e644d1c39f26858dcac320a
ochiaihitomi <sky.force.1013@gmail.com>
2021-02-09 06:00:28 +0000

    [Update] いいね・コメント機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,11 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = @book.favorites.new(user_id: current_user.id)
     @favorite.save
-    
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @favorite = @book.favorites.find_by(user_id: current_user.id)
     @favorite.destroy
-    
   end
 end
