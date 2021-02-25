7e3f5065a5730a820f0267753af9ad725054cc1b
ochiaihitomi <sky.force.1013@gmail.com>
2021-02-25 12:52:56 +0000

    ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.guest
+    find_or_create_by!(name: 'ゲスト', email: 'guest2@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
   include JpPrefecture
   jp_prefecture :prefecture_code
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'search/search'
   devise_for :users
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   root 'homes#top'
   get '/home/about' => 'homes#about'
 
